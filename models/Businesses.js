ce9841d63eebc1d0fac6eec3bb4a3616891eb5b0
jules testard <jtestard@eng.ucsd.edu>
2015-03-23 18:26:02 -0700

    adding elastic search capabilities

--- models/Businesses.js
+++ models/Businesses.js
@@ -1,16 +1,36 @@
 var mongoose = require('mongoose');
+var mongoosastic = require("mongoosastic");
 
 var BusinessSchema = new mongoose.Schema({
 	business_id : String,
-	full_address : String,
+	full_address : { type:String, es_indexed : true },
 	open : Boolean,
 	hours : {},
 	city : String,
 	text : String,
-	name : String,
+	name : { type:String, es_indexed : true},
 	categories : [ String ],
 	attributes : {},
 	stars : Number
+});
+
+BusinessSchema.plugin(mongoosastic);
+
+var Business = mongoose.model('Business', BusinessSchema);
+var stream = Business.synchronize();
+var count = 0;
+
+console.log("Start indexing on elastic search...");
+stream.on('data', function(err, doc){
+  count++;
+  if ((count % 1000) == 0)
+	  console.log("Indexed " + count + " documents...");
+});
+stream.on('close', function(){
+  console.log('indexed ' + count + ' documents!');
+});
+stream.on('error', function(err){
+  console.log(err);
 });
 
 module.exports = mongoose.model('Businesses', BusinessSchema);
--- routes/businesses.js
+++ routes/businesses.js
@@ -6,23 +6,23 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Business = require('../models/Businesses.js');
 
-// Deprecated
-/**
- * GET '/api/businesses' listing. The SQL equivalent is :
- * 
- * 		select *
- * 		from Businesses
- * 		limit 10;
- * 
- */
-router.get('/', function(req, res, next) {
-  var query = Business.find({});
-  query.limit(10);
-  query.exec(function (err, reviews) {
-    if (err) return next(err);
-    res.json(reviews);
-  });
-});
+// Mapping for Elastic Search
+Business.createMapping(function(err, mapping){
+	  if(err){
+	    console.log('error creating mapping (you can safely ignore this)');
+	    console.log(err);
+	  }else{
+	    console.log('mapping created!');
+	    console.log(mapping);
+	  }
+	});
+
+var searchResultFactory = function (res) {
+	return function (err, post) {
+	    if (err) return next(err);
+	    res.json(post);
+	  }
+};
 
 /**
  * post '/api/businesses/' for searching. The SQL equivalent is :
@@ -34,15 +34,31 @@ router.get('/', function(req, res, next) {
  */
 router.post('/', function(req, res, next) {
 	var value = req.body.search;
-	Business.find({ $text : { $search : value } },
-			{ "score": { "$meta": "textScore" } }
-			)
-			.sort({ "score": { "$meta": "textScore" }})
-			.limit(20)
-			.exec(function (err, post) {
-			    if (err) return next(err);
-			    res.json(post);
-			  });
+	var elastic = true; //req.body.elastic;
+	if (!elastic) {
+		Business.find({ $text : { $search : value } },
+				{ "score": { "$meta": "textScore" } }
+				)
+				.sort({ "score": { "$meta": "textScore" }})
+				.limit(20)
+				.exec(function (err, post) {
+				    if (err) return next(err);
+				    res.json(post);
+				  });
+	} else {
+		Business.search(
+				{ 
+					query_string : {
+						fields : [ "name" ], // "full_address" ],
+						query : value
+					}
+				},
+				{ hydrate:true},
+				function (err, post) {
+				    if (err) return next(err);
+				    res.json(post);
+				  });
+	}
 	});
 
 //TODO : write full CRUD specification.
